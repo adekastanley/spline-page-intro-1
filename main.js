a7cd97d9e891ccbe1df008ce3ed20d4d6f4290b1
adeka stanley <adekastanley2@gmail.com>
2023-04-23 22:50:45 +0100

    matchmedia bug for screen sizes

--- main.js
+++ main.js
@@ -12,25 +12,39 @@ for (let i = 0; i < word.length; i++){
 
 
 
-const pageIntro = gsap.timeline()
 
 
+
+// 
+// 
+// general 
+// 
+
+gsap.from('li', {
+    duration: 1,
+    delay: 2,
+    y: -20,
+    opacity: 0,
+    stagger: .5
+})
 const mm = gsap.matchMedia()
+const pageIntro = gsap.timeline()
+////// pc
+//
 
-////mobile
-
-
-mm.add("(max-width: 400px)", ()=>{
+mm.add("(min-width: 1024px)", ()=>{
     pageIntro.from('.tagline h1 span', {
         opacity: 0,
-        duration: 1,
+        duration: .5,
+        ease: 1,
         // y: 500,
         // x: 900
         stagger: .5
     })
     pageIntro.to('.tagline', {
-        duration: 1,
-        x: '-50px',
+        duration: .5,
+        x: '-300%',
+        ease: 1,
         // stagger: .5
     })
     
@@ -40,46 +54,52 @@ mm.add("(max-width: 400px)", ()=>{
 ////// TABLET
 //
 
+mm.add("(min-width: 768px) and (max-width: 1023px)", ()=>{
+    pageIntro.from('.tagline h1 span', {
+        opacity: 0,
+        duration: .5,
+        ease: 1,
+        // y: 500,
+        // x: 900
+        stagger: .5
+    })
+    pageIntro.to('.tagline', {
+        duration: .5,
+        x: '-150%',
+        ease: 1,
+        // stagger: .5
+    })
+    
+})
+
+////mobile
+
+
 mm.add("(min-width: 401px)", ()=>{
     pageIntro.from('.tagline h1 span', {
         opacity: 0,
-        duration: 1,
+        duration: .5,
+        ease: 1,
         // y: 500,
         // x: 900
         stagger: .5
     })
     pageIntro.to('.tagline', {
-        duration: 1,
-        x: '-150%',
+        duration: .5,
+        x: '-50px',
+        ease: 1,
         // stagger: .5
     })
     
 })
 
-////// pc
-//
-
-mm.add("(min-width: 901px)", ()=>{
-    pageIntro.from('.tagline h1 span', {
-        opacity: 0,
-        duration: 1,
-        // y: 500,
-        // x: 900
-        stagger: .5
-    })
-    pageIntro.to('.tagline', {
-        duration: 1,
-        x: '-300%',
-        // stagger: .5
-    })
-    
-})
 
 pageIntro.from('.nav-logo', {
     duration: 1,
     x: '-100%',
     stagger: .5
 })
+
 pageIntro.to('.spline', {
     opacity: 1,
 })
